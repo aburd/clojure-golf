7bc006788bc9866a2601243daac85bcaf409e06a
aburd <aaron.burdick@protonmail.com>
2025-02-01 19:18:24 +0900

    feat: add first activity

---
+++ src/golf/2025/2/search_light.clj
@@ -0,0 +1,13 @@
+(ns golf.2025.2.search-light)
+
+(defn search-light
+  "
+  I should print a string that looks like a search light
+  pointing at the sky!
+
+  Remember, I should print a string, not return one!
+  For an example of what I should print,
+  please look at my spec: golf.2025.2.search-light-test
+  "
+  []
+  "implement me!")
---
+++ test/golf/2025/2/search_light_test.clj
@@ -0,0 +1,37 @@
+(ns golf.2025.2.search-light-test
+  (:require [clojure.test :as t]
+            [golf.2025.2.search-light :as sut]))
+
+
+(def ^:private search-light-s
+  "                         ZYXWVUTSRQPONMLKJIHGFEDCBA
+                        YXWVUTSRQPONMLKJIHGFEDCBA
+                       XWVUTSRQPONMLKJIHGFEDCBA
+                      WVUTSRQPONMLKJIHGFEDCBA
+                     VUTSRQPONMLKJIHGFEDCBA
+                    UTSRQPONMLKJIHGFEDCBA
+                   TSRQPONMLKJIHGFEDCBA
+                  SRQPONMLKJIHGFEDCBA
+                 RQPONMLKJIHGFEDCBA
+                QPONMLKJIHGFEDCBA
+               PONMLKJIHGFEDCBA
+              ONMLKJIHGFEDCBA
+             NMLKJIHGFEDCBA
+            MLKJIHGFEDCBA
+           LKJIHGFEDCBA
+          KJIHGFEDCBA
+         JIHGFEDCBA
+        IHGFEDCBA
+       HGFEDCBA
+      GFEDCBA
+     FEDCBA
+    EDCBA
+   DCBA
+  CBA
+ BA
+A")
+
+(t/deftest search-light-test
+  (t/testing "matches"
+    (t/is (= search-light-s
+             (with-out-str (sut/search-light))))))
