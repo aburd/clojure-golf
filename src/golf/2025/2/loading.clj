32b03075d3ae576bf837c71ebb90096a47556526
aburd <aaron.burdick@protonmail.com>
2025-03-06 11:44:38 +0900

    feat: improve loading challenge

--- src/golf/2025/2/loading.clj
+++ src/golf/2025/2/loading.clj
@@ -1,8 +1,14 @@
 (ns golf.2025.2.loading)
 
-(defn -main []
-  "
-  explanation
+(defn loading 
+  "Take no input.
+
+  Output Loading..., with a trailing space, but no trailing newline.
+
+  Infinitely cycle through the chars |, /, - and \\: every 0.25 seconds, overwrite the last one with the next in the sequence. You can overwrite just the last character, or delete and rewrite the whole line, as long Loading... remains unchanged.
+
+  Explanation with screenshot:
   https://codegolf.stackexchange.com/questions/101289/loading-forever
   "
+  []
   (println "Implement me!"))
